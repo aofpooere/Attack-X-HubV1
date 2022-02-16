61ea15eef0b3137b0187cfd90a55b2e6f084e966
xZcAtliftz <96857831+aofpooere@users.noreply.github.com>
2022-02-16 20:59:49 +0700

    Update AllScript.lua

--- AllScript.lua
+++ AllScript.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/aofpooere/Edit-Script/main/AllScript.lua"), true))()
+_G.Color = Color3.fromRGB(255, 255, 255)
+_G.Toggle = Enum.KeyCode.RightControl
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ErenYeaker/Attack-Hub/main/ScriptBloxFruits.lua"))()
